5a87318e54ee147482d49fab98f1222eddd8339b
vivimila <violamilashevskaya@yandex.by>
2022-08-22 18:37:26 +0300

    Task 38 is solved

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -5,54 +5,49 @@
 
 Console.Write("Введите число элементов массива: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int diffMaxMin = 0;
+int count = 0;
 if (number == 0)
 {
 Console.Write("Вы ввели недопустимое значение");
 return;
 }
-
-
-int[] GetArray(int size, int maxNum, int minNum)
+int[] GetArray(int size)
 {
 int[] array = new int[size];
 Random rnd = new Random();
 for (int i = 0; i < size; i++)
 {
-array[i] = new Random().Next(1,20);
+array[i] = new Random().Next(1,50); // дала ограничение для удобной проверки
 }
 return array;
 }
 
-int GetDiffMaxMin (int[] array)
+    int[] GetDiffMaxMin(int [] array)
 {
-    int maxNum = 0;
-    int minNum = 0;
+    int maxNum = array[0];
+    int minNum = array[0];
+
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] > maxNum) maxNum +=array[i];
-        else maxNum = array[i];
-        if (array[i] < minNum) minNum +=array[i];
-        else minNum = array[i];
+        if(array[i] < array[0]) minNum = array[i];
+        if(array[i] > array[0]) maxNum = array[i];
     }
-    int diffMaxMin = maxNum - minNum;
-    return array;
+    count = maxNum - minNum;
+    return new int[] {count};
 }
 
-int[] result = GetArray(number);
-int result2 = GetDiffMaxMin(array);
+void PrintArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length - 1; i++)
+{
+Console.Write($"{array[i]},");
+}
+Console.WriteLine($"{array[array.Length - 1]}]");
+}
 
+int[] arr = GetArray(number);
+PrintArray(arr);
+int [] diffMaxMin = GetDiffMaxMin(arr);
+Console.Write($"Разница между максимальным и минимальным значаниями в массиве: {count}");
 
-// void PrintArray(int[] array)
-// {
-// Console.Write("[");
-// for (int i = 1; i < array.Length; i++)
-// {
- 
-// Console.Write($"{array[i]},");
-// }
-// Console.Write($"{array[array.Length - 1]}]");
-// }
-
-// PrintArray(result);
-Console.Write($"Количество четных чисел в массиве: {diffMaxMin}");
