e826b0e174f3a09829049e669589c89f3c8badf3
vivimila <violamilashevskaya@yandex.by>
2022-08-22 20:51:19 +0300

    Task 36 is solved

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -5,3 +5,45 @@
 // [-4, -6, 89, 6] -> 0
 
 
+Console.Write("Введите число элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNeg = 0;
+if (number == 0)
+{
+Console.Write("Вы ввели недопустимое значение");
+return;
+}
+int[] GetArray(int size, int min, int max)
+{
+int[] array = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < size; i++)
+{
+array[i] = new Random().Next(-100,100); 
+}
+return array;
+}
+
+    int[] SumNegative(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0) sumNeg = sumNeg + array[i] ;
+    }
+    return new int[] {sumNeg};
+}
+
+void PrintArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length - 1; i++)
+{
+Console.Write($"{array[i]},");
+}
+Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+int[] arr = GetArray(number, 0, 100);
+PrintArray(arr);
+int [] diffMaxMin = SumNegative(arr);
+Console.Write($"Сумма элементов находящихся на нечетных индексах: {sumNeg}");
